c02f049d571889c61ea8ca2772023f8835f84bee
sandrolebon <148054036+sandrolebon@users.noreply.github.com>
2025-03-19 17:07:42 +0100

    la lotta continua

---
+++ phase2/headers/initial.h
@@ -0,0 +1,20 @@
+/* This module implements main() and exports the Nucleus’s global variables */
+//NOTE - vale in ogni file xd -i commenti sono adattati da ivan e notebookLM quindi andranno riscritti (sogno il momento in cui sara' l'ultima cosa da fare)
+#ifndef INITIAL_H
+#define INITIAL_H
+
+#include "../headers/const.h"
+#include "../headers/types.h"
+
+// number of started processes not yet terminated
+int process_count;
+// number of soft-blocked processes 
+int waiting_count;
+// Un intero che può assumere solo i valori 0 e 1, utilizzato per la sincronizzazione tra diverse istanze del Nucleo in esecuzione su CPU differenti
+int global_lock;
+// running process
+pcb_PTR current_process;
+// queue of PCBs in ready state
+struct list_head ready_queue;
+
+#endif
