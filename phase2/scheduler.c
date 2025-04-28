57cdf00766db85b31e99369dfc3430b43cfa9923
sandrolebon <alessandrozanaglia19@gmail.com>
2025-04-28 16:34:31 +0200

    gpt dice SGTM

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -17,27 +17,25 @@ void schedule() {
   ACQUIRE_LOCK(&global_lock);
   int cpu_id = getPRID(); // ottengo l'ID della CPU corrente
   // dispatch the next process
-  pcb_PTR next_process = removeProcQ(&ready_queue);
-
+  pcb_PTR next_process = removeProcQ(&ready_queue); //Rimuovere il PCB da head  della Ready Queue e immagazzinare il puntatore al PCB nel campo Current Process della CPU corrente.
+  RELEASE_LOCK(&global_lock); 
   if (next_process != NULL) {
     // Assegna il processo rimosso all'elemento current_process corrispondente alla CPU corrente
     current_process[cpu_id] = next_process;
-    // load the PLT
-    setTIMER(TIMESLICE * (*((cpu_t *)TIMESCALEADDR)));
+    setTIMER(TIMESLICE * (*((cpu_t *)TIMESCALEADDR)));   // load the PLT
     LDST(&current_process[cpu_id]->p_s); //Load Processor State (LDST) sul processore, carica lo stato salvato nel PCB del processo selezionato, permettendone la ripresa dell'esecuzione
-    RELEASE_LOCK(&global_lock); 
   } else if (process_count == 0) {
-    HALT();//Consider this a job well done!
-  } else if (process_count > 0 && waiting_count > 0) {
-    // waiting for an interrupt
-    //Before executing the WAIT instruction, the Scheduler must first set the mie register to enable interrupts and either disable the PLT (also through the mie register) using:
-    setMIE(MIE_ALL & ~MIE_MTIE_MASK);
-    unsigned int status = getSTATUS();
-    status |= MSTATUS_MIE_MASK;
-    setSTATUS(status); // enable global interrupts and timer interrupt (MTIP)
-    WAIT();
-  } else if (process_count > 0 && waiting_count == 0) {
-    // deadlock
-    PANIC();
-  }
+        HALT();//Consider this a job well done! 
+        } else if (process_count > 0 && waiting_count > 0) {
+          // waiting for an interrupt
+          //Before executing the WAIT instruction, the Scheduler must first set the mie register to enable interrupts and either disable the PLT (also through the mie register) using:
+          setMIE(MIE_ALL & ~MIE_MTIE_MASK); //Disabilita il Local Timer (MTIE) ma lascia abilitati gli interrupt di device esterni (disco, terminale, clock)
+          unsigned int status = getSTATUS();
+          status |= MSTATUS_MIE_MASK; //bilita il bit globale di abilitazione interrupt: senza questo bit a 1, nessun interrupt può interrompere la CPU anche se i singoli interrupt sono abilitati.
+          setSTATUS(status); // enable global interrupts and timer interrupt (MTIP)
+          WAIT();
+          } else if (process_count > 0 && waiting_count == 0) {
+              // deadlock
+              PANIC();
+            }
 }
